4d8bb8056e986eecf4f75011597f77212affab87
Michael Tobias <axis139@gmail.com>
2022-01-10 08:21:27 -0800

    Removes common and functions dirs and moves firebase stuff into web-client

--- common/lib/environment.d.ts
+++
@@ -1,5 +0,0 @@
-export declare enum Environment {
-    Development = 0,
-    Production = 1
-}
-export declare const isDevelopment: boolean;
--- functions/src/firebase.ts
+++
@@ -1,9 +0,0 @@
-import { getApp } from "firebase/app";
-import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
-import { isDevelopment } from 'common'
-
-const functions = getFunctions(getApp());
-
-if (isDevelopment) {
-  connectFunctionsEmulator(functions, "localhost", 5001);
-}
